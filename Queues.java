bae4c0369cf8c05bd66f1da94ec67fc86a46eab9
andwendrew <46432569+andwendrew@users.noreply.github.com>
2022-11-15 11:29:49 -0500

    Update Queues.java

--- Queues.java
+++ Queues.java
@@ -1,125 +1,123 @@
-class Stack
+class Queue
 {
-    private int arr[]; // array to store stack
-    private int top; // variable to track rightmost element of stack
-    private int capacity; // maximum possible size of stack
+    // Here we implement a queue contained within an array, with arr[0] always representing the queue end and the variable front denoting the index of the TypeNotPresentException
+    
+    private int[] arr;      // array to store queue 
+    private int front;      // index pointing to front of queue
+    private int capacity;   // maximum capacity of the queue, i.e maximum size queue can take
  
-    // Constructor of stack, which initializes with size
-    Stack(int size)
+    // Constructor which initializes a queue with given size and initially no elements
+    Queue(int size)
     {
         arr = new int[size];
         capacity = size;
-        top = -1; 
+        front = -1;
     }
  
-    // This method adds (or pushes) an element in from the right
-    public void push(int n)
+    // Method which dequeues element from the front and returns dequeued element
+    public int dequeue()
     {
-        if(top < capacity - 1){ // checks to make sure stack is not full or not
-            top++;
-            arr[top] = n;
-        }
-    }
- 
-    // This method returns (or pops) the rightmost element back out
-    public int pop()
-    {
-        if(top > -1){ // checks to make sure stack is nonempty
-            return arr[top--];
-        }
-        else{
+        if(front == -1) // if queue is already blank, do nothing and return placeholder
+        { 
             return -900;
         }
+        else
+        {
+            front--;
+            return arr[front+1];
+        }
     }
  
-    // Checks rightmost element of stack 
+    // Method which queues in element from the end by shifting everything in the queue up one to the front
+    public void queue(int item)
+    {
+        if (front == capacity - 1) // if queue is full then do nothing
+        {
+            
+        }
+        else
+        {
+            for(int i = front + 1; i >= 1; i--)
+            {
+                arr[i] =  arr[i-1]; // perform the shift
+            }
+            arr[0] = item;
+            front++;
+        }
+    }
+ 
+    // Method to return the front element of the queue
     public int peek()
     {
-        if (top > -1) { // checks stack is nonempty
-            return arr[top];
-        }
-        else{
-            return -900;
-        }
+        return arr[front];
     }
     
-    // Returns size
+    // Method to return size of queue
     public int size()
     {
-        return top + 1;
+        return front + 1;
     }
 }
- 
+
 class Main
-{
-    public static boolean testpeek(int n){ // performs test on peek method by initializing stack of capacity n and random initial size
-        Stack stack = new Stack(n);
-        int rsize = (int) (n*Math.random()); // determine random initial size of stack (cannot be full)
-        for(int i = 0; i < rsize; i++){
-            stack.push((int) (100*Math.random())); // initialize random stack with randomized size
+{   
+    public static boolean testqueuepeek(int n) // jointly tests dequeue and peek methods on queue of capacity n and randomly generated size with nonnegative elements up to 100
+    {
+        Queue q = new Queue(n);
+        
+        int firstqueue = (int) (100*Math.random()); // this should always be returned from peek
+        q.queue(firstqueue);
+        
+        int rsize = (int) (n*Math.random()); // generates random size of queue from empty to one from being full 
+        for(int i = 0; i < rsize; i++) // generates rest of queue randomly
+        { 
+            q.queue((int) (100*Math.random()));
         }
-        int topush = (int) (100*Math.random()); 
-        stack.push(topush);
-        if(stack.peek() == topush){
+        if(q.peek() == firstqueue && q.size() == rsize + 1)
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     
-    public static boolean testpop(int n){ // performs test on pop method by initializing stack of capacity n and random initial size
-        Stack stack = new Stack(n);
-        int rsize = 1 + (int) (n*Math.random()); // determine random initial size of stack (nonempty)
-        for(int i = 0; i < rsize; i++){
-            stack.push((int) (100*Math.random())); // initialize random stack with randomized size
-        }
-        if(stack.peek() == stack.pop()){
-            if(stack.size() == rsize - 1){ // nested criteria checks popped value is last array element and size decreases by 1
-                return true;
-            }
-            return false; 
-        }
-        else{
-            return false;
-        }
-    }
-    
-    public static boolean testpush(int n){ // performs test on pop method by initializing stack of capacity n and random initial size
-        Stack stack = new Stack(n);
-        int rsize = (int) (n*Math.random()); // determine random initial size of stack (not full)
-        for(int i = 0; i < rsize; i++){
-            stack.push((int) (100*Math.random())); // initialize random stack with randomized size
-        }
-        int tobepushed = (int) (100*Math.random());
-        stack.push(tobepushed);
-        if(stack.peek() == tobepushed){ 
-            if(stack.size() == rsize + 1){ // nested criteria checks if pushed value is at top and size increases by 1
-                return true;
-            }
-            return false; 
-        }
-        else{
-            return false;
-        }
-    }
-    
-    public static void main (String[] args)
+    public static boolean testdequeuepeek(int n) // jointly tests queue and peek methods on queue of capacity n and randomly generated size with nonnegative elements up to 100
     {
-        // peek testing - loop through 4 distinct peek tests for distinct n up to 20
-        for(int i = 0; i < 4; i++){
-            System.out.println(testpeek((int) (20*Math.random())));
-        }
+        Queue q = new Queue(n);
         
-        // pop testing - loop through 4 distinct pop tests for distinct n up to 20
-        for(int i = 0; i < 4; i++){
-            System.out.println(testpop((int) (20*Math.random())));
-        }
+        int firstqueue = (int) (100*Math.random()); // first queued in
+        q.queue(firstqueue);
+        int secondqueue = (int) (100*Math.random()); // second queued in
+        q.queue(secondqueue);
         
-        // push testing - loop through 4 distinct push tests for distinct n up to 20
-        for(int i = 0; i < 4; i++){
-            System.out.println(testpush((int) (20*Math.random())));
+        int rsize = (int) ((n-1)*Math.random()); // generates random size of rest of queue from empty to 2 from being full 
+        for(int i = 0; i < rsize; i++) // generates rest of queue randomly
+        { 
+            q.queue((int) (100*Math.random()));
         }
-        
+        if(q.dequeue() == firstqueue && q.peek() == secondqueue) // checks that the element dequeued is indeed the first element queued and the next front is the second element added
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
+    
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < 4; i++)
+        { // performs four distinct tests for randomly generated queues with randomly generated sizes with randomly generated capacities up to 20
+            System.out.println(testqueuepeek((int) (20*Math.random())));
+        }
+        
+        for(int i = 0; i < 4; i++)
+        { // performs four distinct tests for randomly generated queues with randomly generated sizes with randomly generated capacities up to 20
+            System.out.println(testdequeuepeek((int) (20*Math.random())));
+        }
+    }
+    // Source: https://www.javatpoint.com/java-queue
 }
